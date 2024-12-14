dcea190b77fb199721172cff40747cf2a59ba53b
arshitrupanii <arshitrupani6@gmail.com>
2024-12-14 16:14:59 +0530

    mac

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -8,7 +8,6 @@ router.post('/register', [
     body('email').isEmail().withMessage('Invalid email adress..'),
     body('fullname.firstname').isLength({min : 3}).withMessage("first name must be at least 3 charactors long"),
     body('password').isLength({min : 6}).withMessage("Password must be 6 charactors long")
-]
-,usercontroller.registerUser)
+], usercontroller.registerUser)
 
 module.exports = router
